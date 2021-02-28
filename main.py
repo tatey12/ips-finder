8807d8dd41e11f6828669ab384af01b4520882f5
tatey12 <tatebaldwin788@gmail.com>
2021-02-27 19:17:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ global failures
 global passes
 import subprocess
 import time
+import socket
 #How much to sleep between each request (affects all scripts)
 os.environ["WAIT"] = "1"
 imgur = subprocess.Popen(["python","imgur.py"])
 doc = open("PENISES.txt","a")
-os.system('chmod 777 ncat; chmod 777 ping; chmod +s ping;')
+os.system('chmod 777 ncat;')
 def genIP():
     ipGroup = []
     for i in range(4):
@@ -27,20 +28,19 @@ def pingIP(trip,port):
         time.sleep(int(os.environ["WAIT"]))
         ip = genIP()
         print("I Am Do" + str(ip))
-        ping = os.system("./ping " + str(ip))
-        
-        if ping == 1:
+        try:
+            socket.gethostbyaddr(ip)
+        except socket.herror:
             print("EPIC FAIL! for " + ip )
             failures += 1
-        elif ping == 0:
-            print("Oh! And the IP is exist!")
-            print("Time To Check For De ports boi")
-            for i in len(port):
-                penis = os.system("./ncat -w 2 " + str(ip) + " " + str(port[i]))
-                if penis == 1:
-                    print("No Have " + port[i])
-                if penis == 0:
-                    doc.write(ip + ":" + port[i] + "\n")
-                    requests.post("https://canary.discord.com/api/webhooks/815337051293614101/eNTPpsp0YkqlONrRRaYAmTRw4cq03EsOmUHAy7a-DR6vQ6fX_Iu56fQMahT_M2rCXW5k",data={"content":"http://" + str(ip) + port[i]})
-                    passes += 1
+        print("Oh! And the IP is exist!")
+        print("Time To Check For De ports boi")
+        for i in len(port):
+            penis = os.system("./ncat -w 2 " + str(ip) + " " + str(port[i]))
+            if penis == 1:
+                print("No Have " + port[i])
+            if penis == 0:
+                doc.write(ip + ":" + port[i] + "\n")
+                requests.post("https://canary.discord.com/api/webhooks/815337051293614101/eNTPpsp0YkqlONrRRaYAmTRw4cq03EsOmUHAy7a-DR6vQ6fX_Iu56fQMahT_M2rCXW5k",data={"content":"http://" + str(ip) + port[i]})
+                passes += 1
 pingIP(1,["80","443","22","8000","8080","5000"])
